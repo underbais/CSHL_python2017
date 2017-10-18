fcbcd060681af0c3134eee2f28ad30e4777c461a
admin <admin@pfb09.cshl.edu>
2017-10-18 15:56:37 -0400

    set 3 done

--- python3_problemsets/list_copy.py
+++ python3_problemsets/list_copy.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
+# wrong way of creating the copy of a list
+
 my_list = ['a','bb','ccc']
-list_copy = my_list
+list_copy = my_list # doing this you link them and if copy is modified the original will be modified too!!!
 list_copy.append('dddd')
-print('This is copy by assignment',list_copy)
+print('This is the original list', my_list)
+print('This is a modified copy by assignment',list_copy)
+
+# correct way of doing that
 
 my_list = ['a','bb','ccc']
 list_copy = my_list.copy()
 list_copy.append('dddd')
-print('This is copy by .copy method', list_copy)
+print('This is the original list', my_list)
+print('This is a modified copy by .copy method', list_copy)
 
